ac910beb4b881feaf5543401d7cdf29f4882bf3f
SungHyun Park <livealive00@gmail.com>
2016-01-12 19:06:11 +0900

    chapeter 1 done

--- src/scrapetest.py
+++ src/scrapetest.py
@@ -1,5 +1,34 @@
+from urllib.error import HTTPError, URLError
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
-# first app
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-print(html.read())
+
+def getTitle(url):
+
+    try:
+        html = urlopen(url)
+        if html is None:
+            print("URL is not found")
+            return None
+
+    except HTTPError as e:
+        print("HTTP ERROR")
+        print(e)
+        return None
+
+    except URLError as e:
+        print("URL ERROR")
+        print(e)
+        return None
+
+    try:
+        bsObj = BeautifulSoup(html.read(), "html.parser")
+        title =bsObj.body.h1
+    except AttributeError as e:
+        return None
+
+    return title
+
+title = getTitle("http://pythonscraping.com/exercises/exercise1.html")
+
+print(title)
